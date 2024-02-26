c5dca9a6c7fb2ef1e335f7f0a02c74dce0d3d4a7
levijohnson1227 <lw.johnson.1227@gmail.com>
2024-02-26 09:57:07 -0700

    initial commit

---
+++ Nirmata-Mahasura/Assets/SpawnPlayers.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    public void Start()
+    {
+        Vector2 spawnPosition = new Vector2(0,0);
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
+    }
+}
