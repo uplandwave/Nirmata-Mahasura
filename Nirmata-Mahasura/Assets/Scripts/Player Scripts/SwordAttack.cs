101e2bc2530d791a961e513c46a5c21e43567c0d
levijohnson1227 <lw.johnson.1227@gmail.com>
2024-03-26 16:52:27 -0600

    some knights added

---
+++ Nirmata-Mahasura/Assets/Scripts/Player Scripts/SwordAttack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SwordAttack : MonoBehaviour
+{
+    public Animator animator;
+    public PhotonView view;
+
+    public Transform attackPoint;
+    public float attackRange = .05f;
+    public LayerMask enemyLayers;
+
+    public int attackDamage = 40;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (view.IsMine && Input.GetButtonDown("Fire1"))
+        {
+            animator.SetTrigger("Attack");
+            Invoke("Attack", 1);
+        }
+    }
+    void Attack()
+    {
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            //Debug.Log("We Hit " + enemy.name);
+            enemy.GetComponent<Health>().TakeDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
