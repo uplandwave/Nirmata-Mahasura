818a82a1f506c030b4a564a9a1b024f030c47e8a
Torcheek <torcheek@gmail.com>
2024-03-28 18:55:14 -0600

    Camera Finished
    Camera follows player at a good distance.
    
    BIG CHANGE: When a player dies, the camera will reset to be able to see the whole map.  I tried to fit the whole map while not seeing beyond the backgrounds.

--- Nirmata-Mahasura/Assets/Scripts/Camera Scripts/Camera_Controller.cs
+++ Nirmata-Mahasura/Assets/Scripts/Camera Scripts/Camera_Controller.cs
@@ -7,6 +7,14 @@ public class Camera_Controller : MonoBehaviour
 {
     private Transform target;
     public Vector3 offset = new Vector3(0f,0f,-10f);
+
+    private bool player = false;
+
+    public float playerSize;
+
+    public float centerSize;
+
+    [SerializeField] private Camera cam;
   
     void Update()
     {
@@ -18,8 +26,19 @@ public class Camera_Controller : MonoBehaviour
 
     void FindPlayer()
     {
+        try
+        {
         GameObject findPlayer;
         findPlayer = GameObject.FindGameObjectWithTag("Player");     
         target = findPlayer.transform;
+        cam.orthographicSize = playerSize;
+        }
+        catch
+        {
+            GameObject findCenter;
+            findCenter = GameObject.FindGameObjectWithTag("Center");  
+            target = findCenter.transform;
+            cam.orthographicSize = centerSize;  
+        }
     }
 }
